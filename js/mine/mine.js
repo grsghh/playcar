107551915ad6a7248ca71d9c05886f18e7fb3f56
Ray <475868767@qq.com>
2017-01-07 15:29:16 +0800

    update here by Ray

--- js/mine/mine.js
+++ js/mine/mine.js
@@ -7,3 +7,15 @@ $(".aboutUs").on("touchstart",function(){
 	$(".rAboutUs").show();
 	$(".rMask").show();
 })
+//我的礼包跳转
+$(document).on("touchstart",".myGifts",function(){
+	window.location.href = "myGift.html";
+})
+//我的订单跳转
+$(document).on("touchstart",".myOrders",function(){
+	window.location.href = "myGift.html";
+})
+//我的收藏
+$(document).on("touchstart",".myCollections",function(){
+	window.location.href = "collect.html";
+})
